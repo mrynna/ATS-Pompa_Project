d046422b7dc2d8260a3ce22d99f566922f2963c7
Muhammad Riyan Ardiyansyah <muhrianardiansah@gmail.com>
2021-12-14 22:39:24 +0800

    Menambah program Kondisi dan kontrol relay

--- Source Code/sc/sc.ino
+++ Source Code/sc/sc.ino
@@ -8,9 +8,10 @@ void setup(){
     pinMode(ac2, INPUT);
     pinMode(relay1, OUTPUT);
     pinMode(relay2, OUTPUT);
-    digitalWrite(relay1, LOW);
-    digitalWrite(relay2, LOW);
     Serial.begin(9600);
+    delay(1000);
+    digitalWrite(relay1, HIGH);
+    digitalWrite(relay2, LOW);
 }
 void loop(){
     byte tegangan1 = digitalRead(ac1);
@@ -19,13 +20,30 @@ void loop(){
     Serial.println(tegangan1);
     Serial.print("Tegangan Panel = ");
     Serial.println(tegangan2);
-    if(tegangan1 == 1){
+    delay(500);
+    if(tegangan1 == 0 && tegangan2 == 0) {
+        delay(500);
         digitalWrite(relay1, HIGH);
-        digitalWrite(relay2, LOW);
-        Serial.print("Relay 1 Aktif");
-    }else{
-        digitalWrite(relay1, LOW);
+        delay(1000);
         digitalWrite(relay2, HIGH);
-        Serial.print("Relay 2 Aktif");
+        Serial.println("NC");
+    }else if(tegangan1 == 1 && tegangan2 == 0){
+        delay(500);
+        digitalWrite(relay1, HIGH);
+        delay(1000);
+        digitalWrite(relay2, HIGH);
+        Serial.println("NC");
+    }else if(tegangan1 == 1 && tegangan2 == 1){
+        delay(500);
+        digitalWrite(relay1, HIGH);
+        delay(1000);
+        digitalWrite(relay2, LOW);
+        Serial.println("NC");
+    }else{
+        delay(500);
+        digitalWrite(relay2, LOW);
+        delay(1000);
+        digitalWrite(relay1, LOW);
+        Serial.println("Relay 2 Aktif");
     }
 }
