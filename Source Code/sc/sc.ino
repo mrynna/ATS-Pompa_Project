21aa5603e9280602a8abce7a36794671145ac2ab
Muhammad Riyan Ardiyansyah <muhrianardiansah@gmail.com>
2021-12-15 05:08:27 +0800

    Add dc voltage sensor program

--- Source Code/sc/sc.ino
+++ Source Code/sc/sc.ino
@@ -1,11 +1,18 @@
-byte ac1 = 5;
-byte ac2 = 4;
-byte relay1 = 14;
-byte relay2 = 12;
+int ac1 = 5;
+int ac2 = 4;
+int relay1 = 14;
+int relay2 = 12;
+int dcv = A0;
+float vmodul = 0.0; 
+float voltdc = 0.0;
+float R1 = 30000.0; //30k
+float R2 = 7500.0; //7500 ohm resistor
+int avalue = 0;
 
 void setup(){
     pinMode(ac1, INPUT);
     pinMode(ac2, INPUT);
+    pinMode(dcv, INPUT);
     pinMode(relay1, OUTPUT);
     pinMode(relay2, OUTPUT);
     Serial.begin(9600);
@@ -14,6 +21,9 @@ void setup(){
     digitalWrite(relay2, LOW);
 }
 void loop(){
+    avalue = analogRead(dcv);
+    vmodul = (avalue * 5.0) / 1024.0;
+    voltdc = vmodul / (R2/(R1+R2));
     byte tegangan1 = digitalRead(ac1);
     byte tegangan2 = digitalRead(ac2);
     Serial.print("Tegangan PLN = ");
@@ -21,13 +31,13 @@ void loop(){
     Serial.print("Tegangan Panel = ");
     Serial.println(tegangan2);
     delay(500);
-    if(tegangan1 == 0 && tegangan2 == 0) {
+    if((tegangan1 == 0 && tegangan2 == 0) && voltdc >= 10.5) {
         delay(500);
         digitalWrite(relay1, HIGH);
         delay(1000);
         digitalWrite(relay2, HIGH);
         Serial.println("NC");
-    }else if(tegangan1 == 1 && tegangan2 == 0){
+    }else if((tegangan1 == 1 && tegangan2 == 0) && voltdc >= 10.5){
         delay(500);
         digitalWrite(relay1, HIGH);
         delay(1000);
